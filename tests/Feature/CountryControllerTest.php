199a86b8bebb99437945c49c05ead55cf4efcf2f
Davi Menezes <davimenezesoliveira@gmail.com>
2021-04-08 21:05:43 +0200

    Updated the feature tests for the country

--- tests/Feature/CountryControllerTest.php
+++ tests/Feature/CountryControllerTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\Country;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 
@@ -29,5 +31,42 @@ class CountryControllerTest extends TestCase
         $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
         $this->assertInstanceOf(Response::class, $response->baseResponse);
         $this->assertEquals(4, $response->baseResponse->getOriginalContent()->getData()['selectedRegionId']);
+    }
+
+    /**
+     * @covers \App\Http\Controllers\CountryController::editCountryPage
+     */
+    public function testEditCountryPage()
+    {
+        $user = User::all()->first();
+        $this->be($user);
+        $country = Country::all()->first();
+
+        $response = $this->get(route('editCountryPage', ['countryId' => $country->id]));
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertInstanceOf(Response::class, $response->baseResponse);
+        $this->assertEquals($country->id, $response->baseResponse->getOriginalContent()->getData()['country']->id);
+    }
+
+    /**
+     * @covers \App\Http\Controllers\CountryController::updateCountry
+     */
+    public function testUpdateCountry()
+    {
+        $user = User::all()->first();
+        $this->be($user);
+        $country = Country::all()->first();
+
+        $response = $this->put(
+            route('updateCountry', ['countryId' => $country->id]),
+            ["_method" => "PUT", "region" => "Europe", "name" => "name", "code" => "cd", "capital" => "capital"]
+        );
+        $this->assertEquals(Response::HTTP_FOUND, $response->getStatusCode());
+        $this->assertInstanceOf(RedirectResponse::class, $response->baseResponse);
+
+        $updatedCountry = Country::find($country->id);
+        $this->assertEquals("name", $updatedCountry->name);
+        $this->assertEquals("cd", $updatedCountry->code);
+        $this->assertEquals("capital", $updatedCountry->capital);
     }
 }
